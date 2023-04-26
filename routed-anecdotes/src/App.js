13e4dcb2b88b7cdacdeaf01c47f62ea60589a73b
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2023-04-26 19:56:12 +0800

    Finish Part 7.5

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -83,37 +83,43 @@ const CreateNew = (props) => {
   const content = useField('content')
   const author = useField('author')
   const info = useField('info')
-
   const navigation = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content:content.value,
-      author:author.value,
-      info:info.value,
+      content: content.fieldProps.value,
+      author: author.fieldProps.value,
+      info: info.fieldProps.value,
       votes: 0,
     })
     navigation('/')
   }
 
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} onReset={handleReset}>
         <div>
           content
-          <input {...content} />
+          <input {...content.fieldProps} />
         </div>
         <div>
           author
-          <input {...author} />
+          <input {...author.fieldProps} />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input {...info.fieldProps} />
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='reset'>reset</button>
       </form>
     </div>
   )
--- routed-anecdotes/src/hooks/index.js
+++ routed-anecdotes/src/hooks/index.js
@@ -1,13 +1,22 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 export const useField = (type) => {
   const [value, setValue] = useState('')
-  const onChange = (e) => {
-    setValue(e.target.value)
+
+  const onChange = (event) => {
+    setValue(event.target.value)
   }
+
+  const reset = () => {
+    setValue('')
+  }
+
   return {
-    type,
-    value,
-    onChange
+    fieldProps: {
+      type,
+      value,
+      onChange,
+    },
+    reset,
   }
 }
